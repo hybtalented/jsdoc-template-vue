664c908f5b1dda60783d7fa3309874fe86e06a3c
Youbiao He <hybtalented@163.com>
2021-06-03 15:04:16 +0800

    修复开发环境初步配置完成

--- index.js
+++ index.js
@@ -1,61 +1,151 @@
 /* eslint-disable no-console */
 const CLIService = require('@vue/cli-service');
 const path = require('path');
-const Template = require('./template');
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin');
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
+const fs = require('fs');
 
-const resolvePath = pathname => path.resolve(__dirname, pathname);
+let outputFileSystem = null;
+let outputDir;
+const messageHandler = {
+  onmessage: null
+};
+const sendMessage = msg => {
+  console.info('on message ', msg);
+  if (typeof messageHandler.onmessage === 'function') {
+    return messageHandler.onmessage(msg);
+  } else {
+    return false;
+  }
+};
+const readFile = (origin_fs, file) => {
+  try {
+    const fullpath = path.join(outputDir, file);
+    if (typeof origin_fs.readFileSync === 'function') {
+      return origin_fs.readFileSync(fullpath, 'utf-8');
+    } else {
+      return fs.readFileSync(fullpath, 'utf-8');
+    }
+  } catch (e) {
+    return null;
+  }
+};
+const compiler_finish_callback = compile_stats => {
+  const stats = compile_stats.toJson();
+  stats.errors.forEach(err => console.error(err));
+  stats.warnings.forEach(err => console.warn(err));
+  if (stats.errors.length) return;
+  try {
+    const clientManifest = JSON.parse(readFile(outputFileSystem, 'vue-ssr-client-manifest.json'));
+    const serverBundle = JSON.parse(readFile(outputFileSystem, 'vue-ssr-server-bundle.json'));
+    sendMessage({
+      type: 'update',
+      manifest: clientManifest,
+      bundle: serverBundle
+    });
+  } catch (ex) {
+    console.log('unexpect error occur when update ssr manifest and bundle， err', ex);
+  }
+};
 
-const isDevelopment = process.argv[1] === 'serve';
-
-const assetsDir = 'static';
-let ssrBundle;
-let clientManifest;
-let template;
-function updateTemplater() {
-  template = Template;
-}
 /**
  * @type {import("@vue/cli-service").ServicePlugin}
  */
-const servicePlugin = (api, config) => {
-  const readFile = (fs, file) => {
-    try {
-      return fs.readFileSync(path.join(config.outputDir, file), 'utf-8');
-    } catch (e) {}
-  };
-  if (isDevelopment) {
-    if (api.service.mode === 'development') {
-      // dev server
-      const serverService = new CLIService(api.getCwd());
-      serverService.run('build', {
-        mode: 'production',
-        target: 'lib',
-        formats: 'commonjs',
-        watch: true
-      });
+const servicePlugin = api => {
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
-      api.configureDevServer((app, server) => {
-        app.get('*');
-        server.compiler.hooks.done.tap('update ssr client manifest', compile_stats => {
-          const stats = compile_stats.toJson();
-          stats.errors.forEach(err => console.error(err));
-          stats.warnings.forEach(err => console.warn(err));
-          if (stats.errors.length) return;
-          clientManifest = JSON.parse(readFile(server.middleware.fileSystem, 'vue-ssr-client-manifest.json'));
-        });
+  // in ssr bundle mode, add webpack server-bundle-plugin to listen to the change of ssr bundle
+  api.chainWebpack(config => {
+    const isBundle = api.service.mode === 'ssr.bundle';
+    const { output } = config;
+    const ssr_plugin = config.plugin('ssr');
+    const ssr_compiler_plugin = config.plugin('ssr_compiler_inspect');
+
+    if (isBundle) {
+      output.libraryExport('default');
+      ssr_plugin.use(new VueSSRServerPlugin());
+      ssr_compiler_plugin.use({
+        apply: compiler => {
+          // for ssr bundle compiler
+          compiler.outputFileSystem = outputFileSystem;
+          compiler.hooks.done.tap('ssr server bundle plugin', compiler_finish_callback);
+        }
       });
+      // don't optimize for server version
+      config.optimization.splitChunks(false).minimize(false);
+
+      // don't use babel-loader for ssr version
+      config.module.rule('js').uses.delete('babel-loader');
+      config
+        .target('node')
+        .devtool('source-map')
+        .externals(['vue', 'vue-router', 'vuex']);
     } else {
-      // ssr server build
-      api.chainWebpack(config => {
-        config.plugin('server-bundle-plugin').use(resolvePath('server-bundle-plugin'), [
-          newBundle => {
-            ssrBundle = newBundle;
-          },
-          readFile
-        ]);
+      if (isDevelopment) {
+        // in dev mode, enable ssr server
+        api.configureDevServer(app => {
+          app.get('*', (req, res, next) => {
+            const s = Date.now();
+            res.setHeader('Content-Type', 'text/html');
+            const handleError = err => {
+              if (err.url) {
+                res.redirect(err.url);
+              } else if (err.code === 404) {
+                res.status(404).send('404 | Page Not Found');
+              } else {
+                // Render Error Page or Redirect
+                res.status(500).send('500 | Internal Server Error');
+                console.error(`error during render : ${req.url}`);
+                console.error(err.stack);
+              }
+            };
+            const handleResult = html => {
+              res.send(html);
+              console.log(`whole request: ${Date.now() - s}ms`);
+            };
+            const ok = sendMessage({
+              type: 'request',
+              url: req.url,
+              onerror: handleError,
+              onload: handleResult
+            });
+            if (!ok) {
+              next('route');
+            }
+          });
+        });
+      }
+      ssr_plugin.use(new VueSSRClientPlugin());
+      ssr_compiler_plugin.use({
+        apply: compiler => {
+          let bundle_start = false;
+          // for main compiler
+          compiler.hooks.done.tap('ssr client plugin', compiler_stats => {
+            if (!bundle_start) {
+              const serverService = new CLIService(api.getCwd());
+              outputFileSystem = compiler.outputFileSystem;
+              outputDir = compiler.outputPath;
+              serverService.run('build', {
+                mode: 'ssr.bundle',
+                target: 'lib',
+                formats: 'commonjs',
+                watch: isDevelopment,
+                silent: isDevelopment,
+                clean: false,
+                entry: 'src/entry-server.js'
+              });
+              bundle_start = true;
+            }
+            compiler_finish_callback(compiler_stats);
+          });
+        }
       });
     }
-  }
+  });
+};
+
+servicePlugin.messageHandler = {
+  onmessage: null
 };
 
 module.exports = servicePlugin;
--- vue.config.js
+++ vue.config.js
@@ -1,63 +1,24 @@
 const path = require('path');
-const VueSSRServerPlugin = require('vue-server-renderer/server-plugin');
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 
 const resolvePath = pathname => path.resolve(__dirname, pathname);
-
-const rawArgv = process.argv.slice(2);
-const args = require('minimist')(rawArgv, {
-  boolean: [
-    // build
-    'modern',
-    'report',
-    'report-json',
-    'inline-vue',
-    'watch',
-    // serve
-    'open',
-    'copy',
-    'https',
-    // inspect
-    'verbose'
-  ]
-});
-
-const isSsr = args.target === 'lib';
 const assetsDir = 'static';
+const contentOutputDir = 'template';
 /**
  * @type {import("@vue/cli-service").ProjectOptions}
  */
 const vueConfig = {
   publicPath: './',
-  outputDir: resolvePath('template'),
+  outputDir: resolvePath(contentOutputDir),
   assetsDir,
   chainWebpack(config) {
-    const { output, entryPoints } = config;
-    const ssr_plugin = config.plugin('ssr');
-    entryPoints.clear();
-    if (isSsr) {
-      output.libraryExport('default');
-      ssr_plugin.use(new VueSSRServerPlugin());
-      // don't optimize for server version
-      config.optimization.splitChunks(false).minimize(false);
-
-      // don't use babel-loader for ssr version
-      config.module.rule('js').uses.delete('babel-loader');
-      config
-        .target('node')
-        .devtool('source-map')
-        .externals(['vue', 'vue-router', 'vuex']);
-      config.entry('index').add(resolvePath('src/entry-client'));
-    } else {
-      ssr_plugin.use(new VueSSRClientPlugin());
-      if (config.plugins.has('copy')) {
-        config.plugin('copy').tap(args => {
-          args[0][0].to = path.join(config.output.get('path'), assetsDir);
-          return args;
-        });
-      }
-      config.entry('index').add(resolvePath('src/entry-server'));
+    if (config.plugins.has('copy')) {
+      config.plugin('copy').tap(args => {
+        args[0][0].to = path.join(config.output.get('path'), assetsDir);
+        return args;
+      });
     }
+    config.entry('app').clear();
+    config.entry('app').add(resolvePath('src/entry-client'));
   }
 };
 module.exports = vueConfig;
